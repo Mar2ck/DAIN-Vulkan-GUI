96b967321adb4c0e9e8e4a0572527425b5e5576c
Mar2ck <marcchester7@gmail.com>
2020-09-29 20:43:55 +0100

    image_similarity: Set to use GPU by default since module has CPU fallback

--- src/image_similarity.py
+++ src/image_similarity.py
@@ -6,6 +6,7 @@ can be used to detect whether video frames
 are duplicates.
 
 Requires: pillow, SSIM-PIL, progress
+Optional: pyopencl
 """
 # Built-in modules
 import os
@@ -16,7 +17,7 @@ from PIL import Image
 from progress.bar import ShadyBar
 
 
-def CalculateSSIM(image0path, image1path, use_gpu=False):
+def CalculateSSIM(image0path, image1path, use_gpu=True):
     """Calculates SSIM based on two images"""
     # print("Processing:", image0path)
     image0 = Image.open(image0path)
@@ -24,7 +25,7 @@ def CalculateSSIM(image0path, image1path, use_gpu=False):
     return SSIM_PIL.compare_ssim(image0, image1, GPU=use_gpu)
 
 
-def CalculateDirectorySSIM(directoryPath, use_gpu=False, progress=False):
+def CalculateDirectorySSIM(directoryPath, use_gpu=True, progress=False):
     """Calculates the SSIM for every image in a directory
     based on it and the image that precedes it
     """
@@ -54,19 +55,19 @@ if __name__ == "__main__":
     parser.add_argument("-0", "--image0", help="Path of first image")
     parser.add_argument("-1", "--image1", help="Path of second image")
     parser.add_argument("-f", "--folder", help="Path of directory containing images")
-    parser.add_argument("--gpu", action="store_true",
-                        help="Speeds up SSIM calculation via OpenCL")
+    parser.add_argument("--disable-gpu", action="store_true",
+                        help="Force SSIM calculation to use CPU instead of GPU")
     parser.add_argument("-p", "--progress", action="store_true",
                         help="Show progress bar when calculating a directory")
     args = vars(parser.parse_args())
-
+    useGpu = not args["disable_gpu"]
     if not ((args["image0"] and args["image1"]) or args["folder"]):
         parser.error('Requires --image0 and --image1 or --folder')
 
     if (args["image0"] is not None) and (args["image1"] is not None):  # -image0 and -image1
-        SSIM = CalculateSSIM(args["image0"], args["image1"], args["gpu"])
+        SSIM = CalculateSSIM(args["image0"], args["image1"], useGpu)
         print("SSIM: {}".format(SSIM))
     elif args["folder"] is not None:  # -folder
-        directorySSIM = CalculateDirectorySSIM(args["folder"], args["gpu"], args["progress"])
+        directorySSIM = CalculateDirectorySSIM(args["folder"], useGpu, args["progress"])
         for i in sorted(directorySSIM.keys()):
             print("{}: {}".format(i, directorySSIM[i]))
